11449c5aac7fbde903709c4f14225a9fdca2307b
fauguste <frederic.auguste@gmail.com>
2026-01-18 19:14:47 +0100

    Parse JSON response for watermelon API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 )
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 name_on_order = st.text_input("Name on smoothie : ")
