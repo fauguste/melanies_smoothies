2d788213ff34f0e0aa8160d3726b56454389bd85
fauguste <frederic.auguste@gmail.com>
2026-01-18 21:02:39 +0100

    Remove dataframe display and add ingredient selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(
 
 )
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up 5 ingrediants : ",
     my_dataframe,  # Convertir en liste Python
